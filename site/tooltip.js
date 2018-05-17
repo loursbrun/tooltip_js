32fc4787b0d5dfceb0dc6cdc79ac826712db8db2
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-05-17 19:52:52 +0200

    add tootltip js first function

---
+++ site/tooltip.js
@@ -0,0 +1,20 @@
+console.log("Hello Tooltip !!!!");
+
+
+
+
+console.log(document.getElementsByClassName("img-responsive")[1]);
+
+document.getElementsByClassName("img-responsive")[1].style.display = "none";
+
+// var el = document.getElementsByClassName("img-responsive")[1];
+// var body = document.getElementsByTagName('body');
+// el.innerHTML = '<p><a id="clickme" href="#">Click me</a></p>';
+// document.getElementsByClassName("img-responsive")[1].onclick = function (e) {
+//     e.preventDefault();
+//     document.body.innerHTML +='<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;"></div>';
+    
+// }
+
+
+
