0413945ec8129b06d0e3b9779393d8880a3ba58c
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-06-22 17:05:44 +0200

    add css fot tooltip

--- tooltip-position/js/custom.js
+++ tooltip-position/js/custom.js
@@ -23,12 +23,6 @@ function logMessage(msg){
 
 function addTooltips(){
 
-
-// chrome.tabs.executeScript({
-//     code:'document.getElementsByClassName("center")[8].style.display = "none"'
-// });
-
-
     // 1 - Ajout d'un attribut id pour identifier l'élément cible
     chrome.tabs.executeScript({
          code:'document.getElementsByClassName("services-title")[0].setAttribute("id", "tooltip-1")'
@@ -39,22 +33,56 @@ function addTooltips(){
         code:'document.getElementById("tooltip-1").insertAdjacentHTML("beforebegin", "<span id=tooltip-id-1 class=tooltiptext>Tooltip text</span>")'
     });
 
-    // 3 - On ajoute le style au tooltip
-    chrome.tabs.executeScript({
-          code:'document.getElementsByClassName("tooltiptext")[0].style.backgroundColor = "red"'
+    // 3 - On ajoute le style au tooltip 
+
+    // background color
+     chrome.tabs.executeScript({
+          code:'document.getElementsByClassName("tooltiptext")[0].style.backgroundColor = "black"'
      });
+     // color
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.color = "white"'
+     });
+     // width
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.width = "300px"'
+     });
+     // height
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.height = "50px"'
+     });
+     // text-align
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.textAlign = "center"'
+     });
+     // border-radius
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.borderRadius = "6px"'
+     });
+     // padding
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.padding = "5px 0"'
+     });
+     // position
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.position = "absolute"'
+     });
+     // position
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.zIndex = "1"'
+     });
+     // margin-top
+     chrome.tabs.executeScript({
+        code:'document.getElementsByClassName("tooltiptext")[0].style.margin = "-50px 0 0 0"'
+     });
+
+
 
     // 3 - On déplace le tooltip dans l'element cible
     chrome.tabs.executeScript({
         code:'document.getElementById("tooltip-1").appendChild(document.getElementById("tooltip-id-1")'
     });
 
-
-
-
-   
-   //logMessage("json is not valid");
-   
 
 }
 
