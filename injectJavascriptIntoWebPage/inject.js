c171ec6a3915aa9b560fe0dec1934dd7498f1fcb
fbrun <fabien.brun@kcitylabs.fr>
2019-05-24 18:48:42 +0200

    upadate extention

--- injectJavascriptIntoWebPage/inject.js
+++ injectJavascriptIntoWebPage/inject.js
@@ -1,6 +1,6 @@
 // this is the code which will be injected into a given page...
 
-(function() {
+(function () {
 
 	// just place a div at top right
 	var div = document.createElement('div');
@@ -8,7 +8,7 @@
 	div.style.top = 0;
 	div.style.right = 0;
 	div.style.position = 'absolute';
-	div.textContent = 'Injected! 2222';
+	div.textContent = 'Injected! 333';
 	document.body.appendChild(div);
 
 	//alert('inserted self... giggity');
@@ -17,36 +17,38 @@
 	console.log(window.location.pathname);
 
 	let pathName = window.location.pathname;
-        let dataObject = {};
+	let dataObject = {};
 
-        localStorage.getItem(pathName) ? dataObject = JSON.parse(localStorage.getItem(pathName)) : dataObject = {};
+	localStorage.getItem(pathName) ? dataObject = JSON.parse(localStorage.getItem(pathName)) : dataObject = {};
 
-        if (location.search.split('delete=')[1] == "true" && dataObject != {}) {
-            localStorage.removeItem(pathName);
-        }
+	console.log(dataObject);
 
-        let showInfo = false;
-        if (location.search.split('show=')[1] == "true") {
-            showInfo = true;
-        }
+	if (location.search.split('delete=')[1] == "true" && dataObject != {}) {
+		localStorage.removeItem(pathName);
+	}
 
-        //console.log(dataObject);
+	let showInfo = false;
+	if (location.search.split('show=')[1] == "true") {
+		showInfo = true;
+	}
 
-        if (showInfo) {   // windows with statistics clics of elements
-            var output = '';
-            for (var property in dataObject) {
-                output += '<tr ><td style="border-bottom: 1px solid #BFBEBE;">' + property + '</td><td>' + dataObject[property] + '</td></tr> ';
-            }
-            var myLayer = document.createElement('div');
-            myLayer.id = 'bookingLayer';
-            myLayer.style.position = 'absolute';
-            myLayer.style.right = '10px';
-            myLayer.style.top = '10px';
-            myLayer.style.width = '300px';
-            myLayer.style.height = 'auto';
-            myLayer.style.padding = '10px';
-            myLayer.style.background = '#d6d4d4';
-            htmlString = ` 
+	//console.log(dataObject);
+
+	if (showInfo) {   // windows with statistics clics of elements
+		var output = '';
+		for (var property in dataObject) {
+			output += '<tr ><td style="border-bottom: 1px solid #BFBEBE;">' + property + '</td><td>' + dataObject[property] + '</td></tr> ';
+		}
+		var myLayer = document.createElement('div');
+		myLayer.id = 'bookingLayer';
+		myLayer.style.position = 'absolute';
+		myLayer.style.right = '10px';
+		myLayer.style.top = '10px';
+		//myLayer.style.width = '300px';
+		myLayer.style.height = 'auto';
+		myLayer.style.padding = '10px';
+		myLayer.style.background = '#d6d4d4';
+		htmlString = ` 
         <table>
             <thead>
                 <tr>
@@ -58,41 +60,67 @@
             </tbody>
         </table>
         `;
-            myLayer.innerHTML = htmlString;
-            document.body.appendChild(myLayer);
-        }
+		myLayer.innerHTML = htmlString;
+		document.body.appendChild(myLayer);
+	}
 
-        function saveData() {    // save in cookie 
-            localStorage.setItem(pathName, JSON.stringify(dataObject));
-            location.reload();
-        }
+	function saveData() {    // save in cookie 
+		localStorage.setItem(pathName, JSON.stringify(dataObject));
+		//location.reload();
+	}
 
-        function updateDataObject(newValue) {
-            if (dataObject[newValue] == null) { dataObject[newValue] = 0; };
-            dataObject[newValue] = dataObject[newValue] + 1;
-            saveData();
-        }
+	function updateDataObject(newValue) {
+		if (dataObject[newValue] == null) { dataObject[newValue] = 0; };
+		dataObject[newValue] = dataObject[newValue] + 1;
+		saveData();
+	}
 
-        // function listen all click and return ID, Classe name , I dex fo table node
-        document.addEventListener("click", function (eventArgs) {
-            const elementId = eventArgs.target.id;
-            const elementClassName = eventArgs.target.classList[0];
-            if (!elementId && !elementClassName && eventArgs.target.nodeName !== "HTML") {
-                const elementInnerTxt = eventArgs.target.innerHTML;
-                const elementArray = document.getElementsByTagName(eventArgs.target.nodeName);
-                for (let i = 0; i < elementArray.length; i++) {
-                    if (elementArray[i].innerHTML == elementInnerTxt) {
-                        updateDataObject(eventArgs.target.nodeName + "[" + i + "]");
-                    }
-                }
-            } else
-                if (eventArgs.target.id) {
-                    if (eventArgs.target.id != "delete_btn") { updateDataObject(eventArgs.target.id + "_id"); }
-                } else
-                    if (eventArgs.target.classList) {
-                        updateDataObject(eventArgs.target.classList[0] + "_name");
-                    }
-        });
+	// function listen all click and return ID, Classe name , I dex fo table node
+	document.addEventListener("click", function (eventArgs) {
+		const elementId = eventArgs.target.id;
+		const elementClassName = eventArgs.target.classList[0];
+		if (!elementId && !elementClassName && eventArgs.target.nodeName !== "HTML") {
+			const elementInnerTxt = eventArgs.target.innerHTML;
+			const elementArray = document.getElementsByTagName(eventArgs.target.nodeName);
+			for (let i = 0; i < elementArray.length; i++) {
+				if (elementArray[i].innerHTML == elementInnerTxt) {
+					updateDataObject(eventArgs.target.nodeName + "[" + i + "]");
+					console.log(updateDataObject(eventArgs.target.nodeName + "[" + i + "]"));
+				}
+			}
+		} else
+			if (eventArgs.target.id) {
+				if (eventArgs.target.id != "delete_btn") { 
+					updateDataObject(eventArgs.target.id + "_id"); 
+					console.log(updateDataObject(eventArgs.target.id + "_id"));
+				}
+			} else
+				if (eventArgs.target.classList) {
+
+					// console.log(document.getElementsByClassName("cs-nav-item"));
+					// console.log(document.getElementsByClassName("cs-nav-item").length);
+					// console.log(eventArgs.target.innerHTML);
+					// console.log(eventArgs);
+
+					
+
+					const elementInnerTxt = eventArgs.target.innerHTML;
+					const elementArray = document.getElementsByClassName(eventArgs.target.classList);
+					for (let i = 0; i < elementArray.length; i++) {
+						if (elementArray[i].innerHTML == elementInnerTxt) {
+							console.log(eventArgs.target.classList + "[" + i + "]");
+							updateDataObject(eventArgs.target.classList[0] + "[" + i + "]");
+						}
+					}
+
+					//updateDataObject(eventArgs.target.classList[0] + "_name");
+					//console.log(eventArgs.target);
+
+
+
+
+				}
+	});
 
 
 
