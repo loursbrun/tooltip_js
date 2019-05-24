55a047bbc1a6426f28f66fb7e3c9c6ae6277bdcc
fbrun <fabien.brun@kcitylabs.fr>
2019-05-24 18:13:43 +0200

    add js script in extention chrome

--- injectJavascriptIntoWebPage/inject.js
+++ injectJavascriptIntoWebPage/inject.js
@@ -7,12 +7,92 @@
 	div.style.position = 'fixed';
 	div.style.top = 0;
 	div.style.right = 0;
+	div.style.position = 'absolute';
 	div.textContent = 'Injected! 2222';
 	document.body.appendChild(div);
 
 	//alert('inserted self... giggity');
 
 	console.log("******  Injected !!!!!   ******");
+	console.log(window.location.pathname);
+
+	let pathName = window.location.pathname;
+        let dataObject = {};
+
+        localStorage.getItem(pathName) ? dataObject = JSON.parse(localStorage.getItem(pathName)) : dataObject = {};
+
+        if (location.search.split('delete=')[1] == "true" && dataObject != {}) {
+            localStorage.removeItem(pathName);
+        }
+
+        let showInfo = false;
+        if (location.search.split('show=')[1] == "true") {
+            showInfo = true;
+        }
+
+        //console.log(dataObject);
+
+        if (showInfo) {   // windows with statistics clics of elements
+            var output = '';
+            for (var property in dataObject) {
+                output += '<tr ><td style="border-bottom: 1px solid #BFBEBE;">' + property + '</td><td>' + dataObject[property] + '</td></tr> ';
+            }
+            var myLayer = document.createElement('div');
+            myLayer.id = 'bookingLayer';
+            myLayer.style.position = 'absolute';
+            myLayer.style.right = '10px';
+            myLayer.style.top = '10px';
+            myLayer.style.width = '300px';
+            myLayer.style.height = 'auto';
+            myLayer.style.padding = '10px';
+            myLayer.style.background = '#d6d4d4';
+            htmlString = ` 
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="2" style=" background-color: #333;color: #fff;">${pathName}</th>
+                </tr>
+            </thead>
+            <tbody>
+               ${output}
+            </tbody>
+        </table>
+        `;
+            myLayer.innerHTML = htmlString;
+            document.body.appendChild(myLayer);
+        }
+
+        function saveData() {    // save in cookie 
+            localStorage.setItem(pathName, JSON.stringify(dataObject));
+            location.reload();
+        }
+
+        function updateDataObject(newValue) {
+            if (dataObject[newValue] == null) { dataObject[newValue] = 0; };
+            dataObject[newValue] = dataObject[newValue] + 1;
+            saveData();
+        }
+
+        // function listen all click and return ID, Classe name , I dex fo table node
+        document.addEventListener("click", function (eventArgs) {
+            const elementId = eventArgs.target.id;
+            const elementClassName = eventArgs.target.classList[0];
+            if (!elementId && !elementClassName && eventArgs.target.nodeName !== "HTML") {
+                const elementInnerTxt = eventArgs.target.innerHTML;
+                const elementArray = document.getElementsByTagName(eventArgs.target.nodeName);
+                for (let i = 0; i < elementArray.length; i++) {
+                    if (elementArray[i].innerHTML == elementInnerTxt) {
+                        updateDataObject(eventArgs.target.nodeName + "[" + i + "]");
+                    }
+                }
+            } else
+                if (eventArgs.target.id) {
+                    if (eventArgs.target.id != "delete_btn") { updateDataObject(eventArgs.target.id + "_id"); }
+                } else
+                    if (eventArgs.target.classList) {
+                        updateDataObject(eventArgs.target.classList[0] + "_name");
+                    }
+        });
 
 
 
